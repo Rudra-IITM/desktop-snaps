8c4411b76d3c6c7b00f854a5eef7da4fa9137a2f
Rudra Pratap Singh <rudransh.iitm@gmail.com>
2025-01-08 00:43:59 +0530

    Add support for optional README path flag to list and track parts used in README file. (#840)
    * Add support for optional README path flag to list and track parts used more easily in README.
    
    * Better help msg for readme-path arg
    
    * Added precheck for README update markers

--- updatesnap/updatesnapyaml.py
+++ updatesnap/updatesnapyaml.py
@@ -5,6 +5,7 @@
 import sys
 import argparse
 import logging
+import re
 from SnapModule.snapmodule import Snapcraft, Github
 from SnapModule.manageYAML import ManageYAML
 from SnapVersionModule.snap_version_module import is_version_update, is_rock_version_update
@@ -63,9 +64,61 @@ class ProjectManager:
                 data = None
         return data
 
+    def get_readme_file(self, project_url, readme_path):
+        """ Searches in a project for the 'README' file and
+            returns its contents """
+        try:
+            data = self._github.get_file(project_url, readme_path)
+        except (ValueError, ConnectionError):
+            data = None
+        return data
 
-def main():
-    """ Main code """
+    def update_readme(self, project_url, parts, readme_path):
+        """
+        Updates the <!-- Begin Included Components --> to <!-- End Included Components -->
+          sections in the README with the updated part list.
+        """
+        if readme_path is None:
+            return
+
+        readme_data = self.get_readme_file(project_url, readme_path)
+        if not readme_data:
+            print('Failed to get the README file.', file=sys.stderr)
+            sys.exit(1)
+        readme_content = readme_data.decode('utf-8')
+
+        if (
+            "<!-- Begin Included Components -->\n" not in readme_content
+            or "<!-- End Included Components -->" not in readme_content
+        ):
+            print("The required markers are missing in the README file.", file=sys.stderr)
+            return
+
+        parts_contents = "\n".join([
+            f"  - {part['name']} "
+            f"{part['updates'][0]['name'] if part['updates'] else part['version'][0]}"
+            for part in parts if part
+        ])
+        formatted_contents = f"## Included Components\n{parts_contents}"
+        updated_readme = re.sub(
+            r"<!-- Begin Included Components -->\n.*?\n<!-- End Included Components -->",
+            f"<!-- Begin Included Components -->\n"
+            f"{formatted_contents}\n"
+            f"<!-- End Included Components -->",
+            readme_content,
+            flags=re.DOTALL
+        )
+
+        if updated_readme != readme_content:
+            print("Updating README file", file=sys.stderr)
+            with open('readme_output', 'w', encoding="utf8") as readme_output_file:
+                readme_output_file.write(updated_readme)
+        else:
+            print("No updates available for README file", file=sys.stderr)
+
+
+def parse_args():
+    """ Parses command-line arguments for the script. """
     parser = argparse.ArgumentParser(prog='Update Snap YAML',
                                      description='Find the lastest source'
                                      ' versions for snap files and generates a new snapcraft.yaml.')
@@ -79,9 +132,18 @@ def main():
                         help='Version schema of rock repository')
     parser.add_argument('--yaml-path', action='store', default=None,
                         help='Path to the yaml file')
+    parser.add_argument('--readme-path', action='store', default=None,
+                        help='Path to the README.md file where the parts'
+                        'and their version will be listed.')
     parser.add_argument('--verbose', action='store_true', default=False)
     parser.add_argument('project', default='.', help='The project URI')
-    arguments = parser.parse_args(sys.argv[1:])
+
+    return parser.parse_args(sys.argv[1:])
+
+
+def main():
+    """ Main code """
+    arguments = parse_args()
 
     if arguments.project == '.':
         print('A project URI is mandatory', file=sys.stderr)
@@ -127,6 +189,8 @@ def main():
               f" to version '{part['updates'][0]['name']}'", file=sys.stderr)
         version_data['data'] = f"source-tag: '{part['updates'][0]['name']}'"
         has_update = True
+
+    manager.update_readme(arguments.project, parts, arguments.readme_path)
 
     logging.basicConfig(level=logging.INFO)
     if (is_version_update(snap, manager_yaml, arguments, has_update) or
